20fc27ecaaec268066eae528a560a7d0fee404d1
Khulan <khulan.flwr524@gmail.com>
2021-02-06 16:43:08 +0800

    garaas ugsun utgiig avdag bolgono

--- app.js
+++ app.js
@@ -1,37 +1,50 @@
-// User interface controller
+// Дэлгэцтэй ажиллах контроллер
 var uiController = (function() {
- 
-})();
-
-
-// Finance controller
-var financeController = (function() {
-   
-})();
-
-
-// app controller 
-var appController = (function(uCtrl, fCtrl) {
-      var ctrlAddItem = function( ) {
-             // Оруулсан өгөгдлийг дэлгэцнээс олж авах
-
-             // Олж авсан өгөгдлөө санхүүгийн контроллер луу памжуулах, тэнд хадгалах
-
-            // Олж авсан өгөдлүүдээ дэлгэц дээрээ тохирох хэсэгт гаргана
-
-            //Төсвийг тооцоолно
-
-            // Эцсийн үлдэгдэл, тооцоог харуулах
-      }
-
-      document.querySelector('.add__btn').addEventListener('click', function() {
-            ctrlAddItem();
-      })
-
-      document.addEventListener('keypress', function(event) {
-            if(event.keyCode === 13 || event.witch === 13) {
-                  ctrlAddItem();
-            }
-      })
-
-})(uiController, financeController );
+      var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        addBtn: ".add__btn"
+      };
+    
+      return {
+        getInput: function() {
+          return {
+            type: document.querySelector(DOMstrings.inputType).value,
+            description: document.querySelector(DOMstrings.inputDescription).value,
+            value: document.querySelector(DOMstrings.inputValue).value
+          };
+        },
+    
+        getDOMstrings: function() {
+          return DOMstrings;
+        }
+      };
+    })();
+    
+    // Санхүүтэй ажиллах контроллер
+    var financeController = (function() {})();
+    
+    // Програмын холбогч контроллер
+    var appController = (function(uiController, financeController) {
+      var DOM = uiController.getDOMstrings();
+    
+      var ctrlAddItem = function() {
+        // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
+        console.log(uiController.getInput());
+        // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+        // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана
+        // 4. Төсвийг тооцоолно
+        // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
+      };
+    
+      document.querySelector(DOM.addBtn).addEventListener("click", function() {
+        ctrlAddItem();
+      });
+    
+      document.addEventListener("keypress", function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+          ctrlAddItem();
+        }
+      });
+    })(uiController, financeController);
